412b79df2278d4cb577ab6e021da625daadd9e03
UseR <highermath80@gmail.com>
2022-07-16 20:42:59 +0600

    Task 22 Completed

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
-let age =21;
-let userName = "Abir";
-let hobbies = [age, userName];
-let job = {
-    title:'Developer',
-    place:'Bangladesh',
-    salary: 3445
-};
-let adultYear;
-function calculateAdultYears(){
-    adultYear= age-18;
+// 1) Create three new variables:
+//    - A variable that stores the name of an online course of your choice
+//    - A variable that stores the price of that course
+//    - A variable that stores the three main goals that you have, when taking this course
+
+let name='javascript',price=5,goal= ['Learning Javascipt','Becoming a expert','Mastering Javascript'];
+
+//2) Output ("alert") the three variable values
+alert(name);
+alert(price);
+alert(goal);
+
+//3) Try "grouping" the three variables together and still output their values thereafter
+
+let course = {
+    name: 'javascript',
+    price: 5,
+    goal: ['Learning Javascipt','Becoming a expert','Mastering Javascript']
+
 }
-calculateAdultYears();
-alert(adultYear);
+
+alert(course.name);
+alert(course.price);
+alert(course.goal);
+
+//4)Also output the second element in your "main goals" variable
+
+alert(goal[1]);
+
+//5) Add a custom command that does the following:
+//    - Use your "main goals" variable and access an element by its identifier
+//    - The concrete identifier value should be dynamic / flexible
+//      (i.e. the command can be executed for different identifier)
+//    - The "main goals" variable should also be dynamic: The command should work
+//      with ANY list of values
+//    - The custom command should provide the accessed value (i.e. the list element)
+
+function show(list,id){
+    return list[id];
+}
+
+// 6) Execute your custom command from (5) and output ("alert") the result
+
+alert(show(course.goal,1));
