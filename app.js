a92b42a898078292bf215b87486c0587719eb03f
UseR <highermath80@gmail.com>
2022-07-16 21:33:56 +0600

    Function property and method

--- app.js
+++ app.js
@@ -1,44 +1,29 @@
-// 1) Create three new variables:
-//    - A variable that stores the name of an online course of your choice
-//    - A variable that stores the price of that course
-//    - A variable that stores the three main goals that you have, when taking this course
+let age = 21;
+let userName = "Abir";
+let hobbies = ['Sports','Cooking','Racing'];
 
-let name='javascript',price=5,goal= ['Learning Javascipt','Becoming a expert','Mastering Javascript'];
-
-//2) Output ("alert") the three variable values
-alert(name);
-alert(price);
-alert(goal);
-
-//3) Try "grouping" the three variables together and still output their values thereafter
-
-let course = {
-    name: 'javascript',
-    price: 5,
-    goal: ['Learning Javascipt','Becoming a expert','Mastering Javascript']
+let job = {
+    title: 'Developer',
+    place: 'Bangladesh',
+    salary: 400,
 
 }
 
-alert(course.name);
-alert(course.price);
-alert(course.goal);
 
-//4)Also output the second element in your "main goals" variable
-
-alert(goal[1]);
-
-//5) Add a custom command that does the following:
-//    - Use your "main goals" variable and access an element by its identifier
-//    - The concrete identifier value should be dynamic / flexible
-//      (i.e. the command can be executed for different identifier)
-//    - The "main goals" variable should also be dynamic: The command should work
-//      with ANY list of values
-//    - The custom command should provide the accessed value (i.e. the list element)
-
-function show(list,id){
-    return list[id];
+function calculateAdultYear(userAge){
+    let result;
+    result = userAge-18;
+    return result;
 }
 
-// 6) Execute your custom command from (5) and output ("alert") the result
+totalAdultYear = calculateAdultYear(age);
+alert(totalAdultYear);
 
-alert(show(course.goal,1));
+let person = {
+        name: 'Abir',//property
+        greet(){ //method
+            alert('Hello');
+        }
+};
+
+person.greet();
