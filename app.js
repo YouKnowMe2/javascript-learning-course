0d07416ff1eae332fd74408e3f4e53b07b8e42d2
UseR <highermath80@gmail.com>
2022-07-17 12:50:42 +0600

    changing link using javascript (query selector)

--- app.js
+++ app.js
@@ -3,3 +3,9 @@
 //there are two types of accesing the element of the documnet
 
 document.getElementById('external-link').href='https://youtube.com';
+
+//getElementBy is something if we want to access value by their id or class
+
+document.querySelector('a').href='https://udemy.com';
+
+//querySelector for selecting with css selector like p a h1
