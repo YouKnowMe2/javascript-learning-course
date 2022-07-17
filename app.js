514a1abfb05f86ebfc5f82dcfe0396e612ed8f65
UseR <highermath80@gmail.com>
2022-07-17 12:45:37 +0600

    changing link using javascript (query elements)

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-document.body.children[1].children[0].href = 'https://google.com'
+//document.body.children[1].children[0].href = 'https://google.com'
+
+//there are two types of accesing the element of the documnet
+
+document.getElementById('external-link').href='https://youtube.com';
