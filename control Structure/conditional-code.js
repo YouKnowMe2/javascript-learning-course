0186f74c374be1d72ae73386aff372e601d85a32
UseR <highermath80@gmail.com>
2022-07-18 11:00:09 +0600

    Control Structure

---
+++ control Structure/conditional-code.js
@@ -0,0 +1,18 @@
+const  myName = 'Abir';
+
+if(myName ==='Abir'){
+    console.log(myName);
+}
+
+//flag variable
+let isLoogedIn = true;
+
+if(isLoogedIn){
+    console.log('User is looged in');
+}
+
+//Compare  operators ==, ===, >, <. <=, >=, ! !=, !==
+//Equality, => Greater or lesser than => Negation
+
+//logical Operator
+//&& -> and || => or
