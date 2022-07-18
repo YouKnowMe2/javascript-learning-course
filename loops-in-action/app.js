20c6275fdfa5997754153ae7fdbe2a083c42a249
UseR <highermath80@gmail.com>
2022-07-18 13:48:22 +0600

    Rolling the dice

--- loops-in-action/app.js
+++ loops-in-action/app.js
@@ -21,3 +21,44 @@ let displayUser =  document.querySelector('#user-data button');
     }
 }
 displayUser.addEventListener('click',displayData);
+
+
+
+// Statics / Roll the Dice
+
+const rollDiceButtonElement = document.querySelector('#statistics button');
+
+function rollDice(){
+
+    return Math.floor(Math.random()*5) +1;
+}
+
+function deriveNumberOfDiceRolls(){
+    const targetNumber = document.getElementById('user-target-number');
+    const diceRollsListElement = document.getElementById('dice-rolls');
+
+    const enteredNumber = targetNumber.value;
+    diceRollsListElement.innerHTML = '';
+
+    let hasRolledTargetNumber = false;
+    let numberOfRolls =0;
+    while(!hasRolledTargetNumber){
+        const rolledNumber = rollDice();
+        numberOfRolls++;
+        const newRollsListElement = document.createElement('li');
+        const outputtext = 'Roll' + numberOfRolls + ':' + rolledNumber;
+        diceRollsListElement.append(newRollsListElement);
+        diceRollsListElement.append(outputtext);
+        /*if(rolledNumber == enteredNumber){
+            hasRolledTargetNumbr = true;
+        }*/
+        hasRolledTargetNumber = rolledNumber == enteredNumber;
+
+    }
+    const outputTotalRolls = document.getElementById('output-total-rolls');
+    const outputTarget = document.getElementById('output-target-number');
+
+    outputTarget.textContent = enteredNumber;
+    outputTotalRolls.textContent=numberOfRolls;
+}
+rollDiceButtonElement.addEventListener('click',deriveNumberOfDiceRolls);
