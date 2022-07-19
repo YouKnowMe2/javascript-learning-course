592b9902cb185f0b9cb2ae70ed8437d682aa93bf
UseR <highermath80@gmail.com>
2022-07-19 17:19:40 +0600

    Getting player name while clicking on confirm  tic-tac-toe game

--- Tic-Tac-Toe/app.js
+++ Tic-Tac-Toe/app.js
@@ -1,14 +1,17 @@
 const playerConfigElement = document.getElementById('config-overlays');
 const backDropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
 
 const editPlayer1Element = document.getElementById('edit-player-1');
 const editPlayer2Element = document.getElementById('edit-player-2');
 
-const cancelConfigElement = document.getElementById('PlayerConfigElement');
+const cancelConfigElement = document.getElementById('cancel-config');
 
 
 editPlayer1Element.addEventListener('click',openPlayerConfig);
 editPlayer2Element.addEventListener('click',openPlayerConfig);
 
-playerConfigElement.addEventListener('click',closePlayerConfig);
+cancelConfigElement.addEventListener('click',closePlayerConfig);
 backDropElement.addEventListener('click',closePlayerConfig);
+
+formElement.addEventListener('submit',savePlayerConfig);
--- Tic-Tac-Toe/config.js
+++ Tic-Tac-Toe/config.js
@@ -7,3 +7,10 @@ function closePlayerConfig(){
     playerConfigElement.style.display = 'none';
     backDropElement.style.display = 'none';
 }
+
+function savePlayerConfig(event){
+    event.preventDefault();
+    const formData = new FormData(event.target)
+    const enteredPlayerName =formData.get('player_name');
+    console.log(enteredPlayerName);
+}
